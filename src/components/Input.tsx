723b65735ee0483e121008b0c942396a94310011
linzeguang <15817289730@163.com>
2022-12-19 12:09:36 +0800

    add login page

---
+++ src/components/Input.tsx
@@ -0,0 +1,15 @@
+/**
+ * @Author linzeguang
+ * @Date 2022-12-19 12:06:33
+ * @LastEditTime 2022-12-19 12:06:41
+ * @LastEditors linzeguang
+ * @Description
+ */
+
+import type React from "react";
+
+const Input: React.FC = () => {
+  return <input type="text" />;
+};
+
+export default Input;
